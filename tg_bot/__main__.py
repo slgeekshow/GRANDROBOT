f6bbcb7e158bdad8c6fe1ec92f44a26f54761225
slgeekshow <81729180+slgeekshow@users.noreply.github.com>
2021-04-24 15:56:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,7 +26,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-**Hello {}, My Name is {}!** 
+**Hello {}, My Name is {yakari}!** 
 I am an **advance**  group management bot.
 You can find the list of available commands with /help.
 
